73c0693d07a4dc27534985c8f94c81e16c084271
proconsule <34319995+proconsule@users.noreply.github.com>
2021-07-27 09:29:15 +0200

    Updated README and added two vars in config.default.php for future use

--- config.default.php
+++ config.default.php
@@ -7,3 +7,5 @@
 $gameDir = "/var/www/html/switch/data/games"; /* Absolute Files Path, no trailing slash */
 $contentUrl = "/switch/data/games"; /* Files URL, no trailing slash */
 $allowedExtensions = array('nsp', 'xci', 'nsz', 'xcz');
+$netinstall = true; /* Enable/Disable Net Install feature */
+$switchip = "192.168.1.50"; /* Switch IP address for Net Install */
