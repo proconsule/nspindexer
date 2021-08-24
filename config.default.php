e33d4714037858baae577f4ea9fe0ef4a33db8ca
proconsule <34319995+proconsule@users.noreply.github.com>
2021-08-24 13:40:46 +0200

    Added ini_set for memory_limit as suggested by zand this will prevent memory errors as found by some users

--- config.default.php
+++ config.default.php
@@ -4,6 +4,8 @@
  * You should copy this file to 'config.php' to customize it
  */
 
+ini_set('memory_limit', '256M'); /* To prevent low memory errors */
+
 $gameDir = "/var/www/html/switch/data/games"; /* Absolute Files Path, no trailing slash */
 $contentUrl = "/switch/data/games"; /* Files URL, no trailing slash */
 $allowedExtensions = array('nsp', 'xci', 'nsz', 'xcz');
