eeed230611bd7d4112f1a728f5169fe381dae42b
izenn <izenn@users.noreply.github.com>
2022-12-06 13:11:29 -0600

    merge fpiesche b845407bb0c3f788408adcfd9127a7e049b309b9 and 5d6419723b39a265eb3c658fd989c2e8c704febd

--- config.default.php
+++ config.default.php
@@ -4,27 +4,19 @@
  * You should copy this file to 'config.php' to customize it
  */
 
-ini_set('memory_limit', '256M'); /* To prevent low memory errors anyway it may fail if php.ini have a limit set. if so edit php.ini with memory limit >= 256M */
+ini_set('memory_limit', getenv("PHP_MEMORY_LIMIT")?:'256M'); /* To prevent low memory errors anyway it may fail if php.ini have a limit set. if so edit php.ini with memory limit >= 256M */
 
-$gameDir = "/var/www/html/switch/data/games"; /* Absolute Files Path, no trailing slash */
-$contentUrl = "/switch/data/games"; /* Files URL, no trailing slash */
-$allowedExtensions = array('nsp', 'xci', 'nsz', 'xcz');
-$enableNetInstall = true; /* Enable Net Install feature */
-$enableRename = true; /* Enable Rename feature */
-$switchIp = "192.168.1.50"; /* Switch IP address for Net Install */
-$netInstallSrc = false; /* Set to e.g. '192.168.0.1:80' to override source address for Net Install */
+$gameDir = getenv("NSPINDEXER_GAMES_DIR")?:"/var/www/html/switch/data/games"; /* Absolute Files Path, no trailing slash */
+$contentUrl = getenv("NSPINDEXER_CONTENT_URL")?:"/switch/data/games"; /* Files URL, no trailing slash */
+$allowedExtensions = explode(",", getenv("NSPINDEXER_EXTENSIONS")?:'nsp,xci,nsz,xcz');
+$enableNetInstall = getenv("NSPINDEXER_ENABLE_NETINSTALL")?:true; /* Enable Net Install feature */
+$enableRename = getenv("NSPINDEXER_ENABLE_RENAME")?:true; /* Enable Rename feature */
+$switchIp = getenv("NSPINDEXER_SWITCH_IP")?:"192.168.1.50"; /* Switch IP address for Net Install */
+$netInstallSrc = getenv("NSPINDEXER_NETINSTALL_SRC")?:false; /* Set to e.g. '192.168.0.1:80' to override source address for Net Install */
 $showWarnings = true; /* Show configuration warnings on page load */
-
-/* VARS FOR DOCKER USE */
-if(getenv('NSPINDEXER_ABSPATH')){
-$gameDir = getenv('NSPINDEXER_ABSPATH');
-}
-if(getenv('NSPINDEXER_WEBPATH')){
-$contentUrl = getenv('NSPINDEXER_WEBPATH');
-}
 
 /*
  * Advanced Section (use only if you know what you are doing
  */
 
-$keyFile = ""; /* Path to 'prod.keys', must be readable by the webserver/php, but KEEP IT SECURE via .htaccess or similar */
+$keyFile = getenv("NSPINDEXER_KEYFILE")?:""; /* Path to 'prod.keys', must be readable by the webserver/php, but KEEP IT SECURE via .htaccess or similar */
